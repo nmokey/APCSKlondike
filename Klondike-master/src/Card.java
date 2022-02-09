84825d287edb53493e022fb390be5c1a8e8d0700
Ryan Zheng <rz3303@pleasantonusd.net>
2022-02-09 09:54:17 -0800

    test

--- Klondike-master/src/Card.java
+++ Klondike-master/src/Card.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 
-/** represents a playing card that can draw itself. */
-public class Card implements Drawable, Updateable{
+public abstract class Card implements Drawable, Updateable{
+    private int value, suit;
+	public Location loc
+	public Boolean isFaceUp;
+	private static ArrayList<String> valuesSuits = new ArrayList<>(
+			List.of("", "Hearts", "Diamonds", "Clubs", "Spades", "Jack", "Queen", "King", "Ace"));
 
+	public Card(int suit, int value) {
+		this.suit = suit;
+		this.value = value;
+		this.loc = new Location(0,0)
+	}
+
+    public Card(int suit, int value, int x, int y){
+        this.suit=suit;
+        this.value=value;
+		this.loc = new Location(x,y);
+    }
+
+	public int compareTo(Card c) {
+		return this.value-c.value;
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+		if(value<11){
+			s += value;
+		}
+		else{
+			s += valuesSuits.get(value-6);
+		}
+		return s += " of "+valuesSuits.get(suit);
+	}
 }
--- Klondike-master/src/Location.java
+++ Klondike-master/src/Location.java
@@ -1,3 +1,7 @@
 public class Location {
-    
+    private int x, y;
+    public Location(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
 }
